f5a6ae400e088fdfa40e700464a40c81cb38d8cf
Steve Hu <stevehu@gmail.com>
2020-01-24 12:25:10 -0500

    fixes #2 add tracing with mock data and open tracing dependency

---
+++ tracing/src/main/java/com/networknt/ob/handler/DirectDebitsGetHandler.java
@@ -0,0 +1,31 @@
+package com.networknt.ob.handler;
+
+import com.networknt.handler.LightHttpHandler;
+import com.networknt.httpstring.AttachmentConstants;
+import com.networknt.utility.Constants;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.HttpString;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DirectDebitsGetHandler implements LightHttpHandler {
+    
+    @Override
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        Map<String, Object> auditInfo = exchange.getAttachment(AttachmentConstants.AUDIT_INFO);
+        String userId = (String)auditInfo.get(Constants.USER_ID_STRING);
+        String responseBody = null;
+        if("stevehu".equals(userId)) {
+            responseBody = "{\"Data\":{\"DirectDebit\":[{\"AccountId\":\"22289\",\"DirectDebitId\":\"DD03\",\"MandateIdentification\":\"Caravanners\",\"DirectDebitStatusCode\":\"Active\",\"Name\":\"Towbar Club 3 - We Love Towbars\",\"PreviousPaymentDateTime\":\"2017-04-05T10:43:07+00:00\",\"PreviousPaymentAmount\":{\"Amount\":\"0.57\",\"Currency\":\"GBP\"}},{\"AccountId\":\"31820\",\"DirectDebitId\":\"DD77\",\"MandateIdentification\":\"Golfers\",\"DirectDebitStatusCode\":\"Active\",\"Name\":\"Golf Club\",\"PreviousPaymentDateTime\":\"2017-05-06T09:00:00+00:00\",\"PreviousPaymentAmount\":{\"Amount\":\"22.30\",\"Currency\":\"GBP\"}}]},\"Links\":{\"Self\":\"https://api.alphabank.com/open-banking/v3.1/aisp/direct-debits/\"},\"Meta\":{\"TotalPages\":1}}";
+        } else if ("ericbroda".equals(userId)) {
+            responseBody = "{\"Data\":{\"DirectDebit\":[{\"AccountId\":\"42281\",\"DirectDebitId\":\"DD04\",\"MandateIdentification\":\"Caravanners\",\"DirectDebitStatusCode\":\"Active\",\"Name\":\"Towbar Club 3 - We Love Towbars\",\"PreviousPaymentDateTime\":\"2017-04-05T10:43:07+00:00\",\"PreviousPaymentAmount\":{\"Amount\":\"0.57\",\"Currency\":\"GBP\"}},{\"AccountId\":\"41221\",\"DirectDebitId\":\"DD78\",\"MandateIdentification\":\"Golfers\",\"DirectDebitStatusCode\":\"Active\",\"Name\":\"Golf Club\",\"PreviousPaymentDateTime\":\"2017-05-06T09:00:00+00:00\",\"PreviousPaymentAmount\":{\"Amount\":\"22.30\",\"Currency\":\"GBP\"}}]},\"Links\":{\"Self\":\"https://api.alphabank.com/open-banking/v3.1/aisp/direct-debits/\"},\"Meta\":{\"TotalPages\":1}}";
+        }
+        if(responseBody != null) {
+            exchange.getResponseHeaders().add(new HttpString("Content-Type"), "application/json");
+            exchange.getResponseSender().send(responseBody);
+        } else {
+            exchange.setStatusCode(404);
+            exchange.getResponseSender().send("");
+        }
+    }
+}
